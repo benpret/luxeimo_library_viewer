a2d75c0f52e091d490b62da4ce6e946f25028d68
Ben Pretorius <ben.pretorius@imerza.com>
2025-09-10 21:33:34 +0200

    fix: Update library folder validation to include 'models' directory

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function shouldTryLibrary(urlPath) {
   // Accept common top-level library folders (case-insensitive)
   // Includes: assets, materials, mats, and 3d_models (post-rename of Assets)
   // Add a few common variants to be safe.
-  return /^(\/)(assets?|materials?|mats|3d_models|3d-models|3dmodels)\//i.test(urlPath);
+  return /^(\/)(assets?|materials?|mats|models)\//i.test(urlPath);
 }
 
 async function handler(req, res, opts) {
